81ada51d9c3d83f5fbc70311f6969f9dfcfad1e8
Kapil Wankhede <kapilwankhede22@gmail.com>
2023-11-13 09:29:19 +0530

    Update app.py
    cdshfble

--- app.py
+++ app.py
@@ -70,14 +70,21 @@ st.markdown(
 )
 
 st.markdown(
+"""
+**Objective:**
 
-    "
-Objective:
+This project aims to develop a predictive model for determining whether an individual is diabetic or not, 
+utilizing the Pima-Indians diabetes dataset from the UCI repository. Given the binary nature of our target outcome (Yes or No), 
+we will employ supervised classification algorithms. Our goal, informed by existing research and studies 
+(as cited in [this paper](https://www.ijedr.org/papers/IJEDR1703069.pdf)), is to achieve a model classification accuracy of 75% or higher, 
+with a specific emphasis on minimizing false negatives (high recall) in the model.
 
-This project aims to develop a predictive model for determining whether an individual is diabetic or not, utilizing the Pima-Indians diabetes dataset from the UCI repository. Given the binary nature of our target outcome (Yes or No), we will employ supervised classification algorithms. Our goal, informed by existing research and studies (as cited in https://www.ijedr.org/papers/IJEDR1703069.pdf), is to achieve a model classification accuracy of 75% or higher, with a specific emphasis on minimizing false negatives (high recall) in the model.
-
-The focus on minimizing false negatives is crucial to ensure that no individuals with diabetes are overlooked. While some tolerance for false positives is acceptable (instances where non-diabetic individuals are misclassified), the priority is to avoid missing any patients with diabetes. In the event of misclassifying a non-diabetic person as diabetic, they would undergo further testing, ultimately confirming their non-diabetic status."
+The focus on minimizing false negatives is crucial to ensure that no individuals with diabetes are overlooked. While some tolerance for false positives is acceptable
+(instances where non-diabetic individuals are misclassified), the priority is to avoid missing any patients with diabetes. 
+In the event of misclassifying a non-diabetic person as diabetic, they would undergo further testing, ultimately confirming their non-diabetic status.
+"""
 )
+
 
 # Sidebar with user input features
 st.sidebar.header("User Input Features")
